75967884de5a19576a61173798cf63e86b6f7ef0
Nathan Dobrowolski <Nathan.Dobrowolski@nordstrom.com>
2015-01-26 15:44:49 -0800

    yeah'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function Bar() {
 }
 
 Bar.prototype.toHTML = function () {
-    return '<div class="bar">Bar v1.0.3 (red)</div>';
+    return '<div class="bar">Bar v1.0.4 (blue)</div>';
 };
 
 module.exports = Bar;
